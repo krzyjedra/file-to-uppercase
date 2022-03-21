bbd2e5c4c30b8bdd5c41ac8927bee75f78eb00d0
Krzysztof Jędra <k.jedra@adform.com>
2022-03-21 13:56:58 +0100

    first commit

---
+++ fileUppercase.scala
@@ -0,0 +1,16 @@
+import scala.io.Source
+
+object fileUppercase extends App {
+
+  val file = "C:\\projects\\uppercase\\example.txt"
+  val n = 3
+
+  var words = Source.fromFile("C:\\Users\\k.jedra\\Desktop\\testFile.txt").getLines.mkString.split("\\W+")
+  var newWords = List.empty[String]
+
+  for (i <- words.indices) {
+    if (i % n == 0) newWords = newWords :+ words(i).toUpperCase()
+    else newWords = newWords :+ words(i)
+  }
+  newWords.foreach(x => print(x + " "))
+}
